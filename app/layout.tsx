02c7c13329f3c8e06a35945c1dbae5f0fdd580e9
MoheTech <mohehasen690@gmail.com>
2025-07-03 18:28:00 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
-import localFont from "next/font/local"
 import "./globals.css"
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff2",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-})
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff2",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-})
 
 export const metadata: Metadata = {
 	title: 'Mohammed - Developer',
@@ -23,7 +12,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
 	return ( 
 		<html lang="en" className='scroll-smooth'>
 			<body 
-			className={'${geistSans.variable} ${geistMono.variable} antialiased leading-8 overflow-x-hidden dark:bg-darkTheme dark:text-white '}
+			className= "antialiased leading-8 overflow-x-hidden dark:bg-darkTheme dark:text-white "
 			>
 			{children}
 			</body>
