ce7fd08b1c6c758d5bd085a90360b25e06f75855
MoheTech <mohehasen690@gmail.com>
2025-07-03 18:10:05 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,8 @@ import React, { ReactNode } from 'react';
 import { Outfit, Ovo } from 'next/font/google';
 import './globals.css';
 
-const outfit = Outfit({
-	subsets: ['latin'], weight: ['400', '500', '600', '700']
-});
-
-const ovo = Ovo({
-	subsets: ['latin'], weight: ['400']
-});
+const outfit = Outfit({ subsets: ['latin'] })
+const ovo = Ovo({ subsets: ['latin'], weight: '400' })
 
 export const metadata: Metadata = {
 	title: 'Mohammed - Developer',
